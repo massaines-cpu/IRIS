bcf1865fd0554fd31e5ebfa0189a3622c9726e02
inesmassa <inesurvivor@gmail.com>
2026-04-07 11:35:01 +0200

    début front

---
+++ frontend/interface.py
@@ -0,0 +1,10 @@
+#front
+
+import streamlit as st
+
+st.title('interface simple, basique, ordinaire, quelconque, sombre, banale et directe')
+
+st.number_input('longueur de la pétale')
+st.number_input('largeur de la pétale')
+st.number_input('longueur de la sépale')
+st.number_input('largeur de la sépale')
