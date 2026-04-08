6ec530b1d97661f570820e60ac1e3689a825a12b
inesmassa <inesurvivor@gmail.com>
2026-04-08 16:46:39 +0200

    r

--- frontend/interface.py
+++ frontend/interface.py
@@ -3,8 +3,8 @@
 import streamlit as st
 import requests
 from backend.app.main import url
-
-st.title('interface simple, basique, ordinaire, quelconque, sombre, banale et directe', text_alignment="center", color='pink')
+#interface simple, basique, ordinaire, quelconque, sombre, banale et directe
+st.title('preuve maxime manon', text_alignment="center", color='pink')
 
 col1, col2 = st.columns(2)
 
